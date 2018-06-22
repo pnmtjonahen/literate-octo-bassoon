58f01f179f7a25a43eb4361de2c7afdae0bb91f0
Philippe Tjon-A-Hen <philippe@tjonahen.nl>
2018-06-22 12:06:02 +0200

    Update version and added turbine to hystrix dashboard

--- HystrixDashboard/src/main/java/nl/tjonahen/hystrix/HystrixdashboardApplication.java
+++ HystrixDashboard/src/main/java/nl/tjonahen/hystrix/HystrixdashboardApplication.java
@@ -2,10 +2,14 @@ package nl.tjonahen.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableTurbine
+@EnableEurekaClient
 public class HystrixdashboardApplication {
 
 	public static void main(String[] args) {
