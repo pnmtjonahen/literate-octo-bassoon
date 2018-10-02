92d6d8d6befb076ce2c096e5b14af9bc60e11427
Philippe Tjon-A-Hen <philippe@tjonahen.nl>
2018-10-02 10:53:34 +0200

    Compile with jdk11

--- Admin-Server/src/main/java/nl/tjonahen/admin/SpringBootAdminApplication.java
+++ Admin-Server/src/main/java/nl/tjonahen/admin/SpringBootAdminApplication.java
@@ -2,14 +2,13 @@ package nl.tjonahen.admin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableDiscoveryClient
 @EnableAdminServer
 public class SpringBootAdminApplication {
