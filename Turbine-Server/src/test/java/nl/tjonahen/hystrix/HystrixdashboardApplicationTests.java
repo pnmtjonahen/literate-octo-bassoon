76fa5754fca25002901791fd02a871ccf4370181
Philippe Tjon-A-Hen <philippe@tjonahen.nl>
2018-10-02 17:10:00 +0200

    Added eureka server to turbine for testing purposes

--- Turbine-Server/src/test/java/nl/tjonahen/hystrix/HystrixdashboardApplicationTests.java
+++ Turbine-Server/src/test/java/nl/tjonahen/hystrix/HystrixdashboardApplicationTests.java
@@ -3,10 +3,12 @@ package nl.tjonahen.hystrix;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableEurekaServer
 public class HystrixdashboardApplicationTests {
 
 	@Test
