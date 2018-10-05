41c646d4cbcb67089ae6bec4073f6a8a83cc0eba
Philippe Tjon-A-Hen <philippe@tjonahen.nl>
2018-10-05 08:18:18 +0200

    Updates

--- Turbine-Server/src/test/java/nl/tjonahen/hystrix/HystrixdashboardApplicationTests.java
+++ Turbine-Server/src/test/java/nl/tjonahen/hystrix/HystrixdashboardApplicationTests.java
@@ -3,12 +3,10 @@ package nl.tjonahen.hystrix;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@EnableEurekaServer
+@SpringBootTest({"eureka:client:serviceUrl:defaultZone:http://127.0.0.1:8761/eureka/"})
 public class HystrixdashboardApplicationTests {
 
 	@Test
