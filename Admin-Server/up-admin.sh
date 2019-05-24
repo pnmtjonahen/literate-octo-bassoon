8679f4acf4def56980db851c6ded0aca623022f7
Philippe Tjon-A-Hen <philippe@tjonahen.nl>
2019-05-24 10:35:58 +0200

    Cleanup docker and docker config

--- Admin-Server/up-admin.sh
+++ Admin-Server/up-admin.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -p 7983:7983 -p 9091:9090  --network=local-sb-network -e "eureka.client.serviceUrl.defaultZone=http://eurekaserver:8761/eureka/" tjonahen/adminserver
+docker run -p 7983:8080 -p 9091:9090  --network=local-sb-network -e "eureka.client.serviceUrl.defaultZone=http://eurekaserver/eureka/" -e "PROGRAM_OPTS=-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9090 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.local.only=false" tjonahen/adminserver
--- Zipkin-Server/up-zipkin.sh
+++ Zipkin-Server/up-zipkin.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -p 9411:9411 --network=local-sb-network -e "eureka.client.serviceUrl.defaultZone=http://eurekaserver:8761/eureka/" tjonahen/zipkinserver
+docker run -p 9411:9411 --network=local-sb-network -e "eureka.client.serviceUrl.defaultZone=http://eurekaserver/eureka/" tjonahen/zipkinserver
