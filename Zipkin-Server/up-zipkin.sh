213a78b4fb70ada59eec0c3a3b83f20a767e0a50
Philippe Tjon-A-Hen <philippe@tjonahen.nl>
2020-02-04 11:20:11 +0100

    update pom

--- Zipkin-Server/up-zipkin.sh
+++ Zipkin-Server/up-zipkin.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -p 9411:9411 -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro --network=local-sb-network -e "eureka.client.serviceUrl.defaultZone=http://eurekaserver/eureka/" tjonahen/zipkinserver
+docker run -p 9411:9411 -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro --network=local-sb-network -e "eureka.client.serviceUrl.defaultZone=http://eurekaserver/eureka/" --name base_zipkin tjonahen/zipkinserver
